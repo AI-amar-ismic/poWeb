55fe7e83586be8b796b44c6bb32f3c791766fd21
amarismic <amar.ismic@rubicon.ba>
2024-06-10 23:13:44 +0200

    feat: adjust sizes on smaller desktops

--- nip-cms/src/slices/HomeHero/index.tsx
+++ nip-cms/src/slices/HomeHero/index.tsx
@@ -26,85 +26,89 @@ const HomeHero = ({ slice }: HomeHeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
     >
       <div
-        className={styles.imageContainer}
+        className={styles.topContainer}
         style={{
           backgroundImage: `url(${slice.primary.hero_image.url})`,
           backgroundSize: "cover",
         }}
       >
-        <div className={styles.contentContainer}>
-          <PrismicRichText
-            field={slice.primary.hero_title}
-            components={{
-              heading1: ({ children }) => {
-                return <h1 className={styles.title}>{children}</h1>;
-              },
-            }}
-          />
-          <PrismicRichText
-            field={slice.primary.hero_subtitle}
-            components={{
-              paragraph: ({ children }) => {
-                return <p className={styles.subtitle}>{children}</p>;
-              },
-            }}
-          />
-          <div className={styles.buttonsContainer}>
-            <NiPButton
-              variant="tertiaryOutlined"
-              link={slice.primary.primary_button_link}
-            >
-              {slice.primary.primary_button_text}
-            </NiPButton>
-            <NiPButton
-              variant="tertiary"
-              link={slice.primary.secondary_button_link}
-            >
-              {slice.primary.secondary_button_text}
-            </NiPButton>
+        <div className={styles.imageContainer}>
+          <div className={styles.contentContainer}>
+            <PrismicRichText
+              field={slice.primary.hero_title}
+              components={{
+                heading1: ({ children }) => {
+                  return <h1 className={styles.title}>{children}</h1>;
+                },
+              }}
+            />
+            <PrismicRichText
+              field={slice.primary.hero_subtitle}
+              components={{
+                paragraph: ({ children }) => {
+                  return <p className={styles.subtitle}>{children}</p>;
+                },
+              }}
+            />
+            <div className={styles.buttonsContainer}>
+              <NiPButton
+                variant="tertiaryOutlined"
+                link={slice.primary.primary_button_link}
+              >
+                {slice.primary.primary_button_text}
+              </NiPButton>
+              <NiPButton
+                variant="tertiary"
+                link={slice.primary.secondary_button_link}
+              >
+                {slice.primary.secondary_button_text}
+              </NiPButton>
+            </div>
           </div>
         </div>
       </div>
-      <div className={styles.calloutContainer}>
-        <PrismicRichText
-          field={slice.primary.hero_callout}
-          components={{
-            heading1: ({ children }) => {
-              return <h1 className={styles.callout}>{children}</h1>;
-            },
-          }}
-        />
-        <ArrowIcon />
-        <div className={styles.blueSection}>
-          {slice.items.map((item, i) => (
-            <div
-              key={`${item.politike_title}-${i}`}
-              className={styles.blueTextContainer}
-            >
-              <div className={styles.numberTitleContainer}>
-                <h1 className={styles.number}>{i + 1}</h1>
+      <div className={styles.bottomContainer}>
+        <div className={styles.calloutContainer}>
+          <PrismicRichText
+            field={slice.primary.hero_callout}
+            components={{
+              heading1: ({ children }) => {
+                return <h1 className={styles.callout}>{children}</h1>;
+              },
+            }}
+          />
+          <ArrowIcon />
+          <div className={styles.blueSection}>
+            {slice.items.map((item, i) => (
+              <div
+                key={`${item.politike_title}-${i}`}
+                className={styles.blueTextContainer}
+              >
+                <div className={styles.numberTitleContainer}>
+                  <h1 className={styles.number}>{i + 1}</h1>
+                  <PrismicRichText
+                    field={item.politike_title}
+                    components={{
+                      heading2: ({ children }) => {
+                        return <h2 className={styles.title}>{children}</h2>;
+                      },
+                    }}
+                  />
+                </div>
                 <PrismicRichText
-                  field={item.politike_title}
+                  field={item.politike_text}
                   components={{
-                    heading2: ({ children }) => {
-                      return <h2 className={styles.title}>{children}</h2>;
+                    paragraph: ({ children }) => {
+                      return <p className={styles.subText}>{children}</p>;
                     },
                   }}
                 />
+                <PrismicNextLink field={item.procitaj_vise_link}>
+                  <p className={styles.subText}>Pročitaj više</p>
+                </PrismicNextLink>
               </div>
-              <PrismicRichText
-                field={item.politike_text}
-                components={{
-                  paragraph: ({ children }) => {
-                    return <p className={styles.subText}>{children}</p>;
-                  },
-                }}
-              />
-              <PrismicNextLink field={item.procitaj_vise_link}>
-                <p className={styles.subText}>Pročitaj više</p>
-              </PrismicNextLink>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
