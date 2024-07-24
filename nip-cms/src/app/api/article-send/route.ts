6ca0625db6ee8382c76afd5fff74bad5ac277e23
Amar Ismic <73399496+AI-amar-ismic@users.noreply.github.com>
2024-07-24 12:19:16 +0200

    feat: add automatic newsletter for new article

---
+++ nip-cms/src/app/api/article-send/route.ts
@@ -0,0 +1,63 @@
+import type { NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+import { Client } from "@sendgrid/client";
+import mail from "@sendgrid/mail";
+import { createClient } from "@/prismicio";
+import { asText, filter } from "@prismicio/client";
+
+export async function POST(req: NextRequest, res: NextApiResponse) {
+  const secret = process.env.PRISMIC_WEBHOOK_SECRET;
+  const sendgridKey = process.env.SENDGRID_API_KEY;
+  const sendgridTemplateID = process.env.SENDGRID_DYNAMIC_TEMPLATE_ID;
+  const url = `${process.env.SENDGRID_BASE_URL}"/v3/contactdb/recipients"`;
+  const body = await new Response(req.body).json();
+
+  // Check if the secret is correct
+  if (body.secret !== secret) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  // Initiate the Prismic client and fetch the article from the webhook by document ID
+  const prismicClient = createClient();
+  const article = await prismicClient.getByType("clanak", {
+    filters: [filter.at("document.id", body.documents[0])],
+  });
+
+  // Fetch all contacts' emails from the Sendgrid API and place them in an array
+  const client = new Client();
+  client.setApiKey(sendgridKey || "");
+  const results = await client.request({
+    headers: { "Content-Type": "application/json" },
+    url: "/v3/marketing/contacts",
+    method: "GET",
+  });
+  const mailsArray = results[1].result.map((mail: any) => mail.email);
+
+  // Initialize the Sendgrid client and send the email to all contacts
+  mail.setApiKey(sendgridKey || "");
+  const msg = {
+    to: mailsArray,
+    from: "novosti@narodipravda.ba",
+    personalizations: [
+      {
+        to: mailsArray,
+        dynamic_template_data: {
+          saopcenjeImage: article.results[0].data.istaknuta_slika,
+          title: asText(article.results[0].data.naslov),
+          subtitle: asText(article.results[0].data.tekst).slice(0, 100) + "...",
+          link: `https://po-web.vercel.app/vijesti/${article.results[0].uid}`,
+        },
+      },
+    ],
+    template_id: sendgridTemplateID,
+    subject: "Hello world",
+    text: "Hello plain world!",
+    html: "<p>Hello HTML world!</p>",
+  };
+  mail.sendMultiple(msg);
+
+  return NextResponse.json(
+    { message: JSON.stringify(article) },
+    { status: 200 }
+  );
+}
--- nip-cms/src/app/api/newsletter/route.ts
+++ nip-cms/src/app/api/newsletter/route.ts
@@ -1,31 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-// type ResponseData = {
-//   message: string;
-// };
-
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<ResponseData>
-// ) {
-//   const secret = process.env.NEWSLETTER_SECRET;
-//   if (req.headers.authorization !== secret) {
-//     res.status(401).json({ message: "Unauthorized" });
-//   } else {
-//     fetch("https://api.sendgrid.com/v3/marketing/contacts", {
-//       method: "PUT",
-//       headers: {
-//         Authorization:
-//           "Bearer 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",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(req.body),
-//     }).then((response) => console.log(response));
-//   }
-//   res.status(200).json({ message: "Hello from Next.js!" });
-// }
-
 export async function PUT(req: NextRequest, res: NextApiResponse) {
   const secret = process.env.NEXT_PUBLIC_NEWSLETTER_SECRET;
   const sendgridKey = process.env.SENDGRID_API_KEY;
