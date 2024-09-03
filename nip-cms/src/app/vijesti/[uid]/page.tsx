06f85f36c660596e643a3a79fcd3bcaf46b238ac
Amar Ismic <73399496+AI-amar-ismic@users.noreply.github.com>
2024-09-03 18:12:21 +0200

    fix: redirects with dynamic articles

--- nip-cms/src/app/vijesti/[uid]/page.tsx
+++ nip-cms/src/app/vijesti/[uid]/page.tsx
@@ -6,6 +6,8 @@ import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
 import NewsCard from "@/components/NewsCard";
 import { ClanakDocument } from "../../../../prismicio-types";
+import wordpressRedirects from "../../../../redirects";
+import { notFound, permanentRedirect, redirect } from "next/navigation";
 
 type Params = { uid: string };
 
@@ -15,20 +17,31 @@ export async function generateMetadata({
   params: Params;
 }): Promise<Metadata> {
   const client = createClient();
-  const home: any = await client.getByUID("clanak", params.uid);
-  const title = home.data.naslov[0]?.text ?? "";
-  const description = home.data.tekst[0]?.text.slice(0, 150) ?? "";
-  const imageUrl = home.data.istaknuta_slika.url ?? "";
+  let article: any = [];
+  client
+    .getAllByUIDs("clanak", [params.uid])
+    .then((res) => (article = res))
+    .catch(() => console.log("error"));
 
-  return {
-    title: title,
-    description: description,
-    openGraph: {
+  if (article.length === 0) {
+    return {
+      title: "Nepoznata stranica",
+      description: "Nepoznata stranica",
+    };
+  } else {
+    const title = article.data.naslov[0]?.text ?? "";
+    const description = article.data.tekst[0]?.text.slice(0, 150) ?? "";
+    const imageUrl = article.data.istaknuta_slika.url ?? "";
+    return {
       title: title,
       description: description,
-      images: [{ url: imageUrl }],
-    },
-  };
+      openGraph: {
+        title: title,
+        description: description,
+        images: [{ url: imageUrl }],
+      },
+    };
+  }
 }
 
 export default async function Vijest({ params }: { params: Params }) {
@@ -36,100 +49,114 @@ export default async function Vijest({ params }: { params: Params }) {
   //
 
   const client = createClient();
-  const home = await client.getByUID("clanak", params.uid);
-  const relatedArticles = await client.getAllBySomeTags<ClanakDocument>(
-    home.tags,
-    {
-      limit: 3,
-      filters: [filter.at("document.type", "clanak")],
-    }
-  );
-
-  const getDate = () => {
-    const date = new Date(
-      home.data.originalni_datum || home.first_publication_date
+  const home = await client.getAllByUIDs("clanak", [params.uid]);
+  if (home.length > 0) {
+    const relatedArticles = await client.getAllBySomeTags<ClanakDocument>(
+      home[0].tags,
+      {
+        limit: 3,
+        filters: [filter.at("document.type", "clanak")],
+      }
     );
-    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-  };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.articleContainer}>
-          <p
-            className={styles.articleMetaInfo}
-          >{`${getDate()} / PRESS NARODA I PRAVDE`}</p>
-          <div className={styles.article}>
-            <PrismicRichText
-              field={home.data.naslov}
-              components={{
-                heading1: ({ children }) => {
-                  return <h1 className={styles.title}>{children}</h1>;
-                },
-              }}
-            />
-            <PrismicNextImage
-              field={home.data.istaknuta_slika}
-              className={styles.image}
-              priority
-            />
-            <div className={styles.textContainer}>
+    const getDate = () => {
+      const date = new Date(
+        home[0].data.originalni_datum || home[0].first_publication_date
+      );
+      return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+    };
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.articleContainer}>
+            <p
+              className={styles.articleMetaInfo}
+            >{`${getDate()} / PRESS NARODA I PRAVDE`}</p>
+            <div className={styles.article}>
               <PrismicRichText
-                field={home.data.tekst}
+                field={home[0].data.naslov}
                 components={{
                   heading1: ({ children }) => {
                     return <h1 className={styles.title}>{children}</h1>;
                   },
-                  paragraph: ({ children }) => {
-                    return <p className={styles.paragraph}>{children}</p>;
-                  },
-                  strong: ({ children }) => {
-                    return <p className={styles.paragraphStrong}>{children}</p>;
-                  },
-                  em: ({ children }) => {
-                    return <p className={styles.paragraphEm}>{children}</p>;
-                  },
-                  list: ({ children }) => {
-                    return <ul className={styles.list}>{children}</ul>;
-                  },
-                  oList: ({ children }) => {
-                    return <ol className={styles.oList}>{children}</ol>;
-                  },
-                  listItem: ({ children }) => {
-                    return <li className={styles.listItem}>{children}</li>;
-                  },
-                  oListItem: ({ children }) => {
-                    return <li className={styles.oListItem}>{children}</li>;
-                  },
-                  image: ({ node }) => {
-                    return (
-                      <img
-                        className={styles.articleImage}
-                        src={node.url}
-                        alt={node.alt ?? ""}
-                      />
-                    );
-                  },
                 }}
               />
-            </div>
-          </div>
-        </div>
-        {relatedArticles && relatedArticles.length > 0 && (
-          <div className={styles.relatedArticlesContainer}>
-            <h2 className={styles.relatedArticlesTitle}>Vezane novosti</h2>
-            <div className={styles.relatedArticles}>
-              {relatedArticles.map((article, index) => (
-                <NewsCard
-                  news={article}
-                  className={styles.relatedNewsCard}
-                  key={`${index}-${article.id}`}
+              <PrismicNextImage
+                field={home[0].data.istaknuta_slika}
+                className={styles.image}
+                priority
+              />
+              <div className={styles.textContainer}>
+                <PrismicRichText
+                  field={home[0].data.tekst}
+                  components={{
+                    heading1: ({ children }) => {
+                      return <h1 className={styles.title}>{children}</h1>;
+                    },
+                    paragraph: ({ children }) => {
+                      return <p className={styles.paragraph}>{children}</p>;
+                    },
+                    strong: ({ children }) => {
+                      return (
+                        <p className={styles.paragraphStrong}>{children}</p>
+                      );
+                    },
+                    em: ({ children }) => {
+                      return <p className={styles.paragraphEm}>{children}</p>;
+                    },
+                    list: ({ children }) => {
+                      return <ul className={styles.list}>{children}</ul>;
+                    },
+                    oList: ({ children }) => {
+                      return <ol className={styles.oList}>{children}</ol>;
+                    },
+                    listItem: ({ children }) => {
+                      return <li className={styles.listItem}>{children}</li>;
+                    },
+                    oListItem: ({ children }) => {
+                      return <li className={styles.oListItem}>{children}</li>;
+                    },
+                    image: ({ node }) => {
+                      return (
+                        <img
+                          className={styles.articleImage}
+                          src={node.url}
+                          alt={node.alt ?? ""}
+                        />
+                      );
+                    },
+                  }}
                 />
-              ))}
+              </div>
             </div>
           </div>
-        )}
+          {relatedArticles && relatedArticles.length > 0 && (
+            <div className={styles.relatedArticlesContainer}>
+              <h2 className={styles.relatedArticlesTitle}>Vezane novosti</h2>
+              <div className={styles.relatedArticles}>
+                {relatedArticles.map((article, index) => (
+                  <NewsCard
+                    news={article}
+                    className={styles.relatedNewsCard}
+                    key={`${index}-${article.id}`}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    console.log(params.uid);
+    const redirectRoute = wordpressRedirects.find(
+      (redirect) => redirect.source === `/${params.uid}`
+    );
+    if (redirectRoute) {
+      permanentRedirect(`/vijesti/${redirectRoute.destination}`);
+    } else {
+      redirect("/404");
+    }
+  }
 }
